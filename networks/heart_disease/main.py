ed4038af498cec6be5474417f5999432d25a57ce
Artem <artemkush1@gmail.com>
2019-10-25 13:18:25 +0300

    fix

--- networks/heart_disease/main.py
+++ networks/heart_disease/main.py
@@ -6,66 +6,86 @@ import keras
 import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
-from keras.optimizers import RMSprop
+from keras.metrics import Precision, Recall, AUC
 from keras.callbacks import ModelCheckpoint, TerminateOnNaN, ReduceLROnPlateau
+from keras import backend as K
 
 
-data = genfromtxt('heart.csv', delimiter=',')[1:]
+def f1(y_true, y_pred):
+    def recall(y_true, y_pred):
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+        recall = true_positives / (possible_positives + K.epsilon())
+        return recall
 
-print(data.shape)
-
-np.random.shuffle(data)
-
-a = int(len(data) * 0.75)
-
-tr = data[:a]
-te = data[a:]
-
-del data
-
-np.random.shuffle(tr)
-np.random.shuffle(te)
-
-trl = tr[:, 13]
-tr = tr[:, 0:13]
-
-tel = te[:, 13]
-te = te[:, 0:13]
+    def precision(y_true, y_pred):
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + K.epsilon())
+        return precision
+    precision = precision(y_true, y_pred)
+    recall = recall(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
 
 epochs = 1000
 batch_size = 1024
 
-seq = Sequential()
-seq.add(Dense(13, input_dim=13, activation='relu'))
-seq.add(Dense(1, activation='sigmoid'))
+
+def load_data():
+    global tr, trl, te, tel
+    data = genfromtxt('heart.csv', delimiter=',')[1:]
+    print(data.shape)
+    np.random.shuffle(data)
+    a = int(len(data) * 0.75)
+    tr = data[:a]
+    te = data[a:]
+    del data
+    np.random.shuffle(tr)
+    np.random.shuffle(te)
+    trl = tr[:, 13]
+    tr = tr[:, 0:13]
+    tel = te[:, 13]
+    te = te[:, 0:13]
 
 
-seq.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
-history = seq.fit(tr[:], trl[:], batch_size=batch_size,
-                  epochs=epochs, verbose=1,
-                  callbacks=[ModelCheckpoint('weights.hdf5',
-                                             monitor='val_acc',
-                                             verbose=1,
-                                             save_best_only=True),
-                             TerminateOnNaN(),
-                             ReduceLROnPlateau(monitor='val_loss',
-                                               factor=0.4,
-                                               patience=50)],
-                  validation_data=(te[:], tel[:]),
-                  shuffle=True)
+def model():
+    seq = Sequential()
+    seq.add(Dense(13, input_dim=13, activation='relu'))
+    seq.add(Dense(1, activation='sigmoid'))
+    return seq
 
-seq.load_weights(f'weights.hdf5')
-seq.save(f'model')
-print(seq.evaluate(te, tel))
+
+def train(seq):
+    global history
+    seq.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+    # checkpoint = ModelCheckpoint('weights.hdf5',
+    #                             monitor='val_acc',
+    #                             verbose=1,
+    #                             save_best_only=True)
+    history = seq.fit(tr[:], trl[:], batch_size=batch_size,
+                      epochs=epochs, verbose=1,
+                      callbacks=[TerminateOnNaN(),
+                                 ReduceLROnPlateau(monitor='val_loss',
+                                                   factor=0.4,
+                                                   patience=50)],
+                      validation_data=(te[:], tel[:]),
+                      shuffle=True)
+
+    # seq.load_weights(f'weights.hdf5')
+    seq.save(f'model')
+    print(seq.evaluate(te, tel))
 
 
 def ploting():
     # print(history.history.keys())
-    loss = history.history['loss']
-    val_loss = history.history['val_loss']
-    acc = history.history['acc']
-    val_acc = history.history['val_acc']
+    ac = []
+    for i in history.history.keys():
+        ac.append(i)
+    loss = history.history[ac[2]]
+    val_loss = history.history[ac[0]]
+    acc = history.history[ac[3]]
+    val_acc = history.history[ac[1]]
     epochs = range(1, len(loss) + 1)
     fig = plt.figure()
     ax1 = fig.add_subplot(2, 1, 1)
@@ -88,5 +108,11 @@ def ploting():
     plt.show()
 
 
-ploting()
+def main():
+    load_data()
+    m = model()
+    train(m)
+    ploting()
 
+
+main()
