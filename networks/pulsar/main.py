3a9ad1ee91e259f9f387ed67895d603ee225efd3
Artem <artemkush1@gmail.com>
2019-10-30 09:56:03 +0300

    fix

--- networks/pulsar/main.py
+++ networks/pulsar/main.py
@@ -9,6 +9,8 @@ from keras.layers import Dense, Dropout
 from keras.metrics import Recall, Precision, AUC
 from keras.callbacks import ModelCheckpoint, TerminateOnNaN, ReduceLROnPlateau
 from keras import backend as K
+from sklearn.metrics import roc_auc_score
+import tensorflow as tf
 
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor, BaggingRegressor
 
@@ -30,8 +32,33 @@ def f1(y_true, y_pred):
     return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
 
+def roc_auc_score(y_true, y_pred):
+    """ ROC AUC Score.
+    Approximates the Area Under Curve score, using approximation based on
+    the Wilcoxon-Mann-Whitney U statistic.
+    Yan, L., Dodier, R., Mozer, M. C., & Wolniewicz, R. (2003).
+    Optimizing Classifier Performance via an Approximation to the Wilcoxon-Mann-Whitney Statistic.
+    Measures overall performance for a full range of threshold levels.
+    Arguments:
+        y_pred: `Tensor`. Predicted values.
+        y_true: `Tensor` . Targets (labels), a probability distribution.
+    """
+    with tf.name_scope("RocAucScore"):
+        pos = tf.boolean_mask(y_pred, tf.cast(y_true, tf.bool))
+        neg = tf.boolean_mask(y_pred, ~tf.cast(y_true, tf.bool))
+        pos = tf.expand_dims(pos, 0)
+        neg = tf.expand_dims(neg, 1)
+        # original paper suggests performance is robust to exact parameter choice
+        gamma = 0.2
+        p = 3
+        difference = tf.zeros_like(pos * neg) + pos - neg - gamma
+        masked = tf.boolean_mask(difference, difference < 0.0)
+    return tf.reduce_sum(tf.pow(-masked, p))
+
+
 batch_size = 128
 metr = 'acc'  # f1, AUC
+metr = roc_auc_score
 
 
 def load():
@@ -79,7 +106,9 @@ epochs = 100
 def model():
     seq = Sequential()
     seq.add(Dense(16, input_dim=8, activation='relu'))
+    # seq.add(Dropout(0.25))
     seq.add(Dense(8, activation='relu'))
+    # seq.add(Dropout(0.25))
     seq.add(Dense(1, activation='sigmoid'))
     return seq
 
@@ -195,6 +224,6 @@ def main2():
     print(clf.predict(te_data[80:100]))
 
 
-# main()
-main1()
+main()
+# main1()
 # main2()
