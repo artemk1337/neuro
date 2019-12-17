2e71135a8d05a3b3c0b6005dab4e3a52d0e5b184
Artem <artemkush1@gmail.com>
2019-12-17 17:48:35 +0300

    fix

--- networks/prj_donate/parse_user_page.py
+++ networks/prj_donate/parse_user_page.py
@@ -6,22 +6,40 @@ token = "9d480cbc9d480cbc9d480cbc089d26ae6399d489d480cbcc0b0b00ca99b5d870cbf1cb7
 session = vk.Session(access_token=token)  # Авторизация
 vk_api = vk.API(session)
 
-user = vk_api.users.get(user_ids='eridi96', v=5.92,
-                       fields=['sex', 'bdate', 'city', 'country', 'home_town', 'photo_id'])[0]
-sub = vk_api.users.getSubscriptions(user_ids='eridi96', v=5.92)
-print(sub)
-print(user)
-# quit()
-a = vk_api.wall.get(owner_id=user['id'], v=5.92)
-print(len(a['items']))
-quit()
+with open('data/dota2.txt', 'r') as f:
+    data = f.readlines()
+
+arr = []
+for i in data:
+    tmp = i.split('id')[1]
+    tmp = int(tmp.split('\n')[0])
+    arr.append(tmp)
+
+print(arr)
+
+final = {}
+
+for i in range(len(arr)):
+    id = arr[i]
+    max_persons = 1000
+    try:
+        user = vk_api.users.get(user_ids=id, v=5.92,
+                                fields=['sex', 'bdate', 'city', 'country', 'home_town', 'photo_id'])[0]
+        sub = vk_api.users.getSubscriptions(user_id=id, v=5.92)
+        wall = vk_api.wall.get(owner_id=user['id'], v=5.92)
+        d = {'user': user,
+             'wall': wall,
+             'sub': sub}
+        final['id'] = id
+        final['data'] = d
+    except Exception:
+        pass
+    if i >= max_persons:
+        break
+
+
 with open('data/parse.json', 'w') as f:
-    json.dump(a, f, separators=(',', ':'), indent=4)
-
-
-
-
-
+    json.dump(final, f, separators=(',', ':'), indent=4)
 
 
 
