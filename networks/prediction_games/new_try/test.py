0350790d6e6ec9ee385d77d290e19e3765178a6b
Artem <artemkush1@gmail.com>
2020-01-14 23:14:19 +0300

    fix

---
+++ networks/prediction_games/new_try/test.py
@@ -0,0 +1,24 @@
+from hltv.hltv import *
+
+
+"""<==========MANUAL==========>"""
+
+"""
+
+top5teams()
+top30teams()
+top_players()
+get_players(teamid)
+get_team_info(teamid)
+get_matches()
+get_results() - last 100 matchese
+get_results_by_date(start_date, end_date)
+
+"""
+
+
+print(get_matches())
+# print(get_team_info(teamid))
+
+
+
