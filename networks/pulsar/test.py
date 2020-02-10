3dda0501fd64d2d3b1418178308bca33021bd37f
Artem <artemkush1@gmail.com>
2020-02-11 02:45:34 +0300

    fix

--- networks/pulsar/test.py
+++ networks/pulsar/test.py
@@ -62,33 +62,6 @@ seq.add(Dense(1, activation='sigmoid'))"""
 
 
 
-class NeuralNetwork:
-    def ___init__(self, x, y):
-        self.input = x
-        self.weights1 = np.random.rand(self.input.shape[1], 4)
-        self.weights2 = np.random.rand(4, 1)
-        self.y = y
-        self.output = np.zeros(self.y.shape)
-
-    def feedforward(self):
-        self.layer1 = sigmoid(np.dot(self.input, self.weights1))
-        self.output = sigmoid(np.dot(self.layer1, self.weights2))
-
-    def backprop(self):
-        # application of the chain rule to find derivative of the loss function with respect to weights2 and weightsl
-        d_weights2 = np.dot(self.layerl.T,
-                            (2 * (self.y - self.output)
-                             * sigmoid_derivative(self.output)))
-        d_weightsl = np.dot(self.input.T,
-                            (np.dot(
-                                2 * (self.y - self.output) * sigmoid_derivative(self.output),
-                                self.weights2.T)
-                             * sigmoid_derivative(self.layerl)))
-        # update the weights with the derivative (slope) of the loss function
-        self.weightsl += d_weightsl
-        self.weights2 += d_weights2
-
-
 
 
 
